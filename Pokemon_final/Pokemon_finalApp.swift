07b6c010fee5d42213d1e5fedf54afd1ff07fb08
Kaj Schermer Didriksen <kdidriksen@me.com>
2022-03-09 09:29:23 +0100

    Initial Commit

---
+++ Pokemon_final/Pokemon_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_finalApp.swift
+//  Pokemon_final
+//
+//  Created by ksd on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
